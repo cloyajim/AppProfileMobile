fa8feae008938ac6d98f0a3fa333c4ac7306b715
Carlos Loya <cloyajim@gmail.com>
2022-10-08 21:46:03 -0500

    Actualización Fragment

--- app/src/main/java/com/cursosandroidant/profile/SettingsActivity.kt
+++ app/src/main/java/com/cursosandroidant/profile/SettingsActivity.kt
@@ -2,10 +2,28 @@ package com.cursosandroidant.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.title = getString(R.string.settings_title)
+        }
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container_main, SettingsFragment())
+            .commit()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == android.R.id.home){
+            onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
